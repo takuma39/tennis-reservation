8c6efdd58d645fb7fae85b04a5ed2332ec721788
takuma39 <takuma.sato.job@gmail.com>
2020-07-07 15:56:40 +0000

    Add enum記載　＆　新規登録作成

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,10 @@
 class Reservation < ApplicationRecord
+
+	enum status:{
+    入金待ち: 0,
+    入金確認: 1,
+    キャンセル: 2,
+    強制キャンセル: 3,
+  	}
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+ 　　devise :database_authenticatable, :registerable,
+       　　 :recoverable, :rememberable, :validatable
+
+
+	enum gender:{
+    男: 0,
+    女: 1,
+  	}
+
+	enum gender:{
+    NORMAL: 0,
+    SILVER: 1,
+    GOLD: 2,
+    VIP: 3,
+  	}
+
 end
