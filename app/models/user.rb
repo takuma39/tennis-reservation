943374a50b9b7e86739bd0917911a23c77ca550a
takuma39 <takuma.sato.job@gmail.com>
2020-07-08 04:02:48 +0000

    Fix user.ub sytaxエラー解決

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
- 　　devise :database_authenticatable, :registerable,
-       　　 :recoverable, :rememberable, :validatable
-
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
 	enum gender:{
     男: 0,
