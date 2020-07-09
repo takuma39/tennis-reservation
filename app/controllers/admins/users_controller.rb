44c803713a359bdcd1d05ae18665850875c166c2
takuma39 <takuma.sato.job@gmail.com>
2020-07-09 08:11:25 +0000

    Add admin/user機能追加　＆　Fix user/user機能修正　＆　Fix admin/ヘッダー修正　＆　コード整え

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -3,18 +3,50 @@ class Admins::UsersController < ApplicationController
 	before_action :authenticate_admin!
 
 	def index
+		@users = User.all
 	end
 
 	def show
+		@user = User.find(params[:id])
 	end
 
 	def edit
+		@user = User.find(params[:id])
 	end
 
 	def search
 	end
 
 	def update
+		@user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:notice] = "更新完了"
+        redirect_to admins_user_path(@user.id)
+      else
+        render :edit
+      end
 	end
+
+
+  private
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :name_kana,
+      :gender,
+      :birthdat,
+      :postcode,
+      :prefecture_name,
+      :address_city,
+      :address_town,
+      :address_street,
+      :address_building,
+      :telephone_number,
+      :rank,
+      :penalty,
+      :active,
+      :remark
+      )
+  end
 
 end
