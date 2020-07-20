219d2a990dfe7b04baedb5cb7efb6b8fcccda933
takuma39 <takuma.sato.job@gmail.com>
2020-07-20 07:24:09 +0000

    FIx admin/コート写真機能修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '475a2f6fe74a59712cc2f370f64d08e4016c10fe694864afa2bbf3ed9ff0a4355aa68a119206592d6642885e5c1a7c58af41385e391f899fa1d0362d095763a2'
